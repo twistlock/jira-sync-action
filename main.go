c3a8d8f451f50d0bc16e0629abae6d878d9f8a3b
Liron Levin <llevin@paloaltonetworks.com>
2021-02-23 08:19:50 +0200

    Use the correct JIRA users
    JIRA user search API might return partial match, check all users to
    ensure correct user is assigned.

--- main.go
+++ main.go
@@ -184,10 +184,16 @@ func syncIssue(config *jiraConfig, githubIssue *githubIssue) error {
 			return fmt.Errorf("failed to find user %s %w", string(body), err)
 			return err
 		}
-		if len(jiraUsers) > 0 {
-			users[loginID] = &jiraUsers[0]
+		for i := range jiraUsers { // Ensure we got the correct user
+			if strings.Contains(strings.ToLower(jiraUsers[i].DisplayName), strings.ToLower(name)) {
+				users[loginID] = &jiraUsers[i]
+				fmt.Println("asd", users[loginID])
+				break
+			}
 		}
 	}
+
+	return nil
 
 	// Create issue with the same title as GH issue
 	title := stringify(githubIssue.issue.Title)
